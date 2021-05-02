4ac0075c45f910a8326357c07bd536d3f9e86037
ugurkarakurt <ugurkarakurt57@gmail.com>
2021-05-02 19:37:31 +0300

    #12 - Feat : Audio added.

--- Sidebar_Menu/assets/js/app.js
+++ Sidebar_Menu/assets/js/app.js
@@ -3,19 +3,31 @@ const sideBar = document.querySelector(".sidebar")
 const navLinks = document.querySelectorAll(".nav-link");
 const background = document.getElementById("background");
 const cancel = document.getElementById("cancel");
+var audio = document.getElementById("myAudio");
+
 
 navLinks.forEach(link => {
     link.addEventListener("click", function () {
         navLinks.forEach(link => {
             link.classList.remove("active");
-
         });
         this.classList.add("active");
         sideBar.style.setProperty('--background', `${this.name}`);
         background.src = `./assets/images/${this.id}.png`;
         cancel.style.color = this.name;
+        audio.src = `./assets/audio/${this.id}.mp3`;
+        pauseAudio()
+        playAudio()
 
     });
 
 
 });
+
+function playAudio() {
+    audio.play();
+}
+
+function pauseAudio() {
+    audio.pause();
+}
