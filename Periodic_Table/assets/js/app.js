50f77449095d1e09ebec50512495522f6e5dd3f4
Uğur Karakurt <ugurkarakurt57@gmail.com>
2021-09-28 23:19:31 +0300

    ### Project finished

--- Periodic_Table/assets/js/app.js
+++ Periodic_Table/assets/js/app.js
@@ -1,6 +1,4 @@
 import { UI } from "./ui.js";
-import { allElementsInformation } from "./elements.js";
-
 const ui = new UI();
 
 // Setup module
@@ -14,30 +12,92 @@ const periodicTableBasic = (function () {
   //Variables
   const elements = document.querySelectorAll(".element");
   const changeButtons = document.querySelectorAll(".changeCheck");
-  const checkState = "all";
+  const search = document.querySelector(".search");
+  const elementNames = Array.from(document.querySelectorAll(".name"));
 
   // Hover Effect
   const _hoverEffectOnElements = function () {
     ui.hoverEffectOnElementsUI(elements);
   };
 
-  // Information Modals
+  // MouseOut Event
+  const _mouseoutEffectOnElements = function () {
+    ui.mouseoutEffectOnElementsUI(elements, changeButtons);
+  };
+
+  // Element info
   const _informationForElement = function () {
     elements.forEach((element) => {
       element.addEventListener("click", function () {
-        ui.informationForElementUI(elements);
+        document.querySelector(".input__description").style.display = "none";
+        ui.informationForElementUI(element);
+        _closeModalEvent();
+        search.value = "";
       });
     });
   };
 
   // Change element check state
-
   const _changeCheck = function () {
     changeButtons.forEach((button) => {
       button.addEventListener("click", () => {
-        ui.changeCheckUI(elements, button);
+        ui.changeCheckUI(elements, button, changeButtons);
       });
     });
+  };
+
+  // Close Modal Event
+  const _closeModalEvent = function () {
+    const closeButton = document.querySelector(".close__modal");
+    const infoModal = document.getElementById("infoModal");
+    const keyESC = 27;
+
+    infoModal.addEventListener("click", function (e) {
+      if (e.target.id === infoModal.id) {
+        ui.closeModalEventFromUI(infoModal);
+        document.querySelector(".input__description").style.display = "block";
+      }
+    });
+
+    closeButton.addEventListener("click", function () {
+      ui.closeModalEventFromUI(infoModal);
+      search.focus();
+      document.querySelector(".input__description").style.display = "block";
+    });
+
+    document.addEventListener("keydown", function (event) {
+      if (event.keyCode === keyESC) {
+        ui.closeModalEventFromUI(infoModal);
+        search.focus();
+        document.querySelector(".input__description").style.display = "block";
+      }
+    });
+  };
+
+  //Empty search
+  const _removeSearch = function () {
+    document.addEventListener("keyup", function (e) {
+      if (e.keyCode === 27) {
+        search.value = "";
+        elements.forEach((element) => {
+          element.classList.remove("active");
+          element.classList.remove("grayscale");
+        });
+      }
+    });
+  };
+
+  // Search An Element
+  const _searchElement = function () {
+    document.addEventListener("click", function (e) {
+      const main = document.querySelector(".main");
+      const emptySpacer = document.querySelector(".empty-spacer");
+      if (e.target === main || emptySpacer) {
+        search.focus();
+      }
+    });
+
+    ui.searchElementUI(elements, elementNames, search);
   };
 
   //
@@ -47,8 +107,11 @@ const periodicTableBasic = (function () {
   return {
     init: function () {
       _hoverEffectOnElements();
+      _mouseoutEffectOnElements();
       _informationForElement();
       _changeCheck();
+      _searchElement();
+      _removeSearch();
     },
   };
 })();
--- Periodic_Table/assets/js/ui.js
+++ Periodic_Table/assets/js/ui.js
@@ -1,8 +1,25 @@
+import { allElementsInformation } from "./elements.js";
 export class UI {
+  mouseoutEffectOnElementsUI(elements, changeButtons) {
+    elements.forEach((element) => {
+      element.addEventListener("mouseout", function () {
+        changeButtons.forEach((btn) => {
+          btn.style.transform = "scale(1)";
+        });
+      });
+    });
+  }
+
   hoverEffectOnElementsUI(elements) {
     elements.forEach((element) => {
       element.addEventListener("mouseover", function () {
-        const className = element.className.split(" ").slice(1).join(" ");
+        const className = element.className
+          .split(" ")
+          .slice(1)
+          .join(" ")
+          .trim();
+        const activeBTN = document.querySelector(`.${className}__info`);
+        activeBTN && (activeBTN.style.transform = "scale(1.1)");
         switch (className) {
           case "alkali__metal":
             elements.forEach((element) => {
@@ -83,8 +100,15 @@ export class UI {
       });
     });
   }
-  changeCheckUI(elements, button) {
-    const className = button.className.split(" ").slice(1).join(" ");
+
+  changeCheckUI(elements, button, buttons) {
+    let className = button.className.split(" ").slice(1).join(" ");
+    buttons.forEach((btn) => {
+      !button.classList.contains("status__info") &&
+        (btn.style.transform = "scale(1)");
+    });
+    !button.classList.contains("status__info") &&
+      (button.style.transform = "scale(1.1)");
 
     switch (className) {
       case "alkali__metal__info":
@@ -94,7 +118,7 @@ export class UI {
             : (element.style.filter = "grayscale(90%)");
         });
         break;
-      case "alkaline__metal__earth__info":
+      case "alkaline__earth__metal__info":
         elements.forEach((element) => {
           element.classList.contains("alkaline__earth__metal")
             ? (element.style.filter = "grayscale(0%)")
@@ -115,7 +139,7 @@ export class UI {
             : (element.style.filter = "grayscale(90%)");
         });
         break;
-      case "lanthande__info":
+      case "lanthanide__info":
         elements.forEach((element) => {
           element.classList.contains("lanthanide")
             ? (element.style.filter = "grayscale(0%)")
@@ -158,5 +182,145 @@ export class UI {
         });
         break;
     }
+  }
+
+  informationForElementUI(element) {
+    const elementNumber = Number(element.nextElementSibling.innerHTML.trim());
+    const elementNumberConvert = elementNumber - 1;
+
+    const main = document.querySelector("main");
+
+    main.style.filter = "blur(1.1rem)";
+    this.informationForElementModal(
+      allElementsInformation[elementNumberConvert]
+    );
+  }
+
+  closeModalEventFromUI() {
+    infoModal.style.display = "none";
+    const main = document.querySelector("main");
+    main.style.filter = "blur(0)";
+  }
+
+  informationForElementModal(element) {
+    const infoModal = document.getElementById("infoModal");
+    infoModal.style.display = "flex";
+    infoModal.innerHTML = "";
+    infoModal.innerHTML = `
+    <div class="box drop_menu_animation_half_second">
+        <div class="modal-content">
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Name :</th>
+                        <td> ${element.name}</td>
+                    </tr>
+                    <tr>
+                        <th>Type :</th>
+                        <td>${element.type}</td>
+                    </tr>
+                    <tr>
+                        <th>Atomic Number :</th>
+                        <td>${element.atomic_number}</td>
+                    </tr>
+                    <tr>
+                        <th>Atomic Mass :</th>
+                        <td>${element.atomic_mass}</td>
+                    </tr>
+                    <tr>
+                        <th>Electronegativity According to Pauling :</th>
+                        <td>${element.electronegativity_according_to_pauling}</td>
+                    </tr>
+                    <tr>
+                        <th>Density :</th>
+                        <td>${element.density}</td>
+                    </tr>
+                    <tr>
+                        <th>Melting Point :</th>
+                        <td>${element.melting_point}</td>
+                    </tr>
+                    <tr>
+                        <th>Boiling Point :</th>
+                        <td>${element.boiling_point}</td>
+                    </tr>
+                    <tr>
+                        <th>Vanderwaal Radius :</th>
+                        <td>${element.vanderwaals_radius}</td>
+                    </tr>
+                    <tr>
+                        <th>Ionic Radius :</th>
+                        <td>${element.ionic_radius}</td>
+                    </tr>
+                    <tr>
+                        <th>Isotopes :</th>
+                        <td>${element.isotopes}</td>
+                    </tr>
+                    <tr>
+                        <th>Electronic Shell :</th>
+                        <td>${element.electronic_shell}</td>
+                    </tr>
+                    <tr>
+                        <th>Energy of First Ionisation :</th>
+                        <td>${element.energy_of_first_ionisation}</td>
+                    </tr>
+                    <tr>
+                        <th>Discovered By :</th>
+                        <td>${element.discovered_by}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <span class="atomic__number" title="Atomic Number">11</span>
+        <a class="close__modal" title="Close Modal">&#10005;</a>
+    </div>
+                            `;
+  }
+
+  searchElementUI(elements, elementNames, input) {
+    input.addEventListener("input", function () {
+      const titles = document.querySelectorAll(".changeCheck");
+      titles.forEach((title) => {
+        title.classList.remove("active");
+        title.classList.add("grayscale");
+      });
+      elements.forEach((element) => {
+        element.classList.remove("active");
+        element.classList.add("grayscale");
+      });
+      let filterResult = [];
+      elementNames.filter((item) => {
+        item.innerHTML
+          .toLocaleLowerCase()
+          .trim()
+          .includes(this.value.toLocaleLowerCase().trim()) &&
+          filterResult.push(item.innerHTML);
+      });
+      if (filterResult && filterResult.length <= 118) {
+        for (let i = 0; i < filterResult.length; i++) {
+          const filteredElement = document.querySelector(
+            `[data-name="${filterResult[i].toLocaleLowerCase()}"]`
+          );
+          if (filteredElement) {
+            filteredElement.classList.add("active");
+            filteredElement.classList.remove("grayscale");
+            let group =
+              filteredElement.className
+                .replace("active", "")
+                .replace("element", "")
+                .trim() + "__info";
+            const selectedClassName = document.querySelector("." + group);
+            selectedClassName.classList.add("active");
+            selectedClassName.classList.remove("grayscale");
+          }
+        }
+      } else {
+        elements.forEach((element) => {
+          element.classList.remove("grayscale");
+          titles.forEach((title) => {
+            title.classList.remove("grayscale");
+          });
+        });
+      }
+    });
   }
 }
